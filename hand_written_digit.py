abac10628eb2e4efde5559694d1de7339f21cf4c
SUPRIYA G <44086885+supriyaggithub@users.noreply.github.com>
2020-06-23 16:10:03 +0530

    Add files via upload

---
+++ hand_written_digit.py
@@ -0,0 +1,232 @@
+
+# coding: utf-8
+
+# In[4]:
+
+
+# deeper cnn model for mnist
+from numpy import mean
+from numpy import std
+from matplotlib import pyplot
+from sklearn.model_selection import KFold
+from keras.datasets import mnist
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.optimizers import SGD
+
+
+# In[5]:
+
+
+def load_dataset():
+	# load dataset
+	(trainX, trainY), (testX, testY) = mnist.load_data()
+	# reshape dataset to have a single channel
+	trainX = trainX.reshape((trainX.shape[0], 28, 28, 1))
+	testX = testX.reshape((testX.shape[0], 28, 28, 1))
+	# one hot encode target values
+	trainY = to_categorical(trainY)
+	testY = to_categorical(testY)
+	return trainX, trainY, testX, testY
+
+
+# In[6]:
+
+
+# scale pixels
+def prep_pixels(train, test):
+	# convert from integers to floats
+	train_norm = train.astype('float32')
+	test_norm = test.astype('float32')
+	# normalize to range 0-1
+	train_norm = train_norm / 255.0
+	test_norm = test_norm / 255.0
+	# return normalized images
+	return train_norm, test_norm
+
+
+# In[7]:
+
+
+# define cnn model
+def define_model():
+	model = Sequential()
+	model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(28, 28, 1)))
+	model.add(MaxPooling2D((2, 2)))
+	model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform'))
+	model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform'))
+	model.add(MaxPooling2D((2, 2)))
+	model.add(Flatten())
+	model.add(Dense(100, activation='relu', kernel_initializer='he_uniform'))
+	model.add(Dense(10, activation='softmax'))
+	# compile model
+	opt = SGD(lr=0.01, momentum=0.9)
+	model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+	return model
+
+
+# In[8]:
+
+
+# evaluate a model using k-fold cross-validation
+def evaluate_model(dataX, dataY, n_folds=5):
+	scores, histories = list(), list()
+	# prepare cross validation
+	kfold = KFold(n_folds, shuffle=True, random_state=1)
+	# enumerate splits
+	for train_ix, test_ix in kfold.split(dataX):
+		# define model
+		model = define_model()
+		# select rows for train and test
+		trainX, trainY, testX, testY = dataX[train_ix], dataY[train_ix], dataX[test_ix], dataY[test_ix]
+		# fit model
+		history = model.fit(trainX, trainY, epochs=10, batch_size=32, validation_data=(testX, testY), verbose=0)
+		# evaluate model
+		_, acc = model.evaluate(testX, testY, verbose=0)
+		print('> %.3f' % (acc * 100.0))
+		# stores scores
+		scores.append(acc)
+		histories.append(history)
+	return scores, histories
+
+
+# In[9]:
+
+
+# plot diagnostic learning curves
+def summarize_diagnostics(histories):
+	for i in range(len(histories)):
+		# plot loss
+		pyplot.subplot(2, 1, 1)
+		pyplot.title('Cross Entropy Loss')
+		pyplot.plot(histories[i].history['loss'], color='blue', label='train')
+		pyplot.plot(histories[i].history['val_loss'], color='orange', label='test')
+		# plot accuracy
+		pyplot.subplot(2, 1, 2)
+		pyplot.title('Classification Accuracy')
+		pyplot.plot(histories[i].history['accuracy'], color='blue', label='train')
+		pyplot.plot(histories[i].history['val_accuracy'], color='orange', label='test')
+	pyplot.show()
+
+
+# In[10]:
+
+
+# summarize model performance
+def summarize_performance(scores):
+	# print summary
+	print('Accuracy: mean=%.3f std=%.3f, n=%d' % (mean(scores)*100, std(scores)*100, len(scores)))
+	# box and whisker plots of results
+	pyplot.boxplot(scores)
+	pyplot.show()
+
+
+# In[11]:
+
+
+# run the test harness for evaluating a model
+def run_test_harness():
+	# load dataset
+	trainX, trainY, testX, testY = load_dataset()
+	# prepare pixel data
+	trainX, testX = prep_pixels(trainX, testX)
+	# evaluate model
+	scores, histories = evaluate_model(trainX, trainY)
+	# learning curves
+	summarize_diagnostics(histories)
+	# summarize estimated performance
+	summarize_performance(scores)
+
+
+# In[14]:
+
+
+def run_test_harness():
+	# load dataset
+	trainX, trainY, testX, testY = load_dataset()
+	# prepare pixel data
+	trainX, testX = prep_pixels(trainX, testX)
+	# define model
+	model = define_model()
+	# fit model
+	model.fit(trainX, trainY, epochs=10, batch_size=32, verbose=0)
+	# save model
+	model.save('final_model.h5')
+
+
+# In[16]:
+
+
+def run_test_harness():
+	# load dataset
+	trainX, trainY, testX, testY = load_dataset()
+	# prepare pixel data
+	trainX, testX = prep_pixels(trainX, testX)
+	# load model
+	model = load_model('final_model.h5')
+	# evaluate model on test dataset
+	_, acc = model.evaluate(testX, testY, verbose=0)
+	print('> %.3f' % (acc * 100.0))
+
+
+# In[15]:
+
+
+# entry point, run the test harness
+run_test_harness()
+
+
+# In[18]:
+
+
+# entry point, run the test harness
+from keras.models import load_model
+run_test_harness()
+
+
+# In[12]:
+
+
+
+# entry point, run the test harness
+run_test_harness()
+
+
+# In[24]:
+
+
+# make a prediction for a new image.
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.models import load_model
+
+# load and prepare the image
+def load_image(filename):
+	# load the image
+	img = load_img(filename, grayscale=True, target_size=(28, 28))
+	# convert to array
+	img = img_to_array(img)
+	# reshape into a single sample with 1 channel
+	img = img.reshape(1, 28, 28, 1)
+	# prepare pixel data
+	img = img.astype('float32')
+	img = img / 255.0
+	return img
+
+# load an image and predict the class
+def run_example():
+	# load the image
+	img = load_image(r'C:\Users\Supriya G\Desktop\SUPRIYA B E\ML internship\Project\Hand_written_digit\sample_image_1.png')
+	# load model
+	model = load_model('final_model.h5')
+	# predict the class
+	digit = model.predict_classes(img)
+	print(digit[0])
+
+# entry point, run the example
+run_example()
+
